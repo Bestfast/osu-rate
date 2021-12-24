4660b87d4639bfd660dd056986afb1a62524604c
Bestfast <bestfast03@gmail.com>
2021-12-24 16:50:19 +0100

    removed hardcoded path, improved the readme.MD

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ import requests
 from reamber.osu.OsuMap import OsuMap
 
 r = requests.get('http://127.0.0.1:24050/json')
-json = json.loads(r.text)['menu']['bm']['path']
+jsons = json.loads(r.text)['menu']['bm']['path']
 
-SONG_FOLDER = 'E:\\Games\\osu!\\Songs\\'
-FOLDER = SONG_FOLDER + json['folder'] + '\\'
+SONG_FOLDER = json.loads(r.text)['settings']['folders']['songs']
+FOLDER = f"{SONG_FOLDER}\\{jsons['folder']}\\"
 RATE = float(sys.argv[1])
 
-m = OsuMap.readFile(FOLDER + json['file'])
+m = OsuMap.readFile(FOLDER + jsons['file'])
 m.rate(RATE, True)
 
 for offset in m.bpms:
-    offset.bpm = offset.bpm * RATE
+    offset.bpm *= RATE
 
-m.version = m.version + " {}x".format(RATE)
-m.audioFileName = json['audio'][:-4] + "{}.mp3".format(RATE)
-m.writeFile(FOLDER + json['file'][:-4] + " {}.osu".format(RATE))
+m.version = f"{m.version} {RATE}x {round(int(m.bpms[0].bpm), 1)}bpm"
+m.audioFileName = jsons['audio'][:-4] + "{}.mp3".format(RATE)
+m.writeFile(FOLDER + jsons['file'][:-4] + " {}.osu".format(RATE))
 
-os.system(f'ffmpeg -i "{FOLDER + json["audio"]}" -filter_complex [0:a]atempo={RATE}[s0] -map [s0] "{FOLDER + m.audioFileName}"') # TODO: Don't use os.system+ffmpeg
+os.system(f'ffmpeg -i "{FOLDER + jsons["audio"]}" -filter_complex [0:a]atempo={RATE}[s0] -map [s0] "{FOLDER + m.audioFileName}"')
